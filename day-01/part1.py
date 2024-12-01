ec155a4f0fca9f23e5a75df107fab744e552acad
olivier <olibitter@gmail.com>
2024-12-01 09:46:26 +0100

    day-01: solve part 1

--- day-01/part1.py
+++ day-01/part1.py
@@ -1,5 +1,15 @@
+def read_input(filename: str) -> tuple[list[int], ...]:
+    with open(filename) as file:
+        lines = [tuple(map(int, line.split())) for line in file]
+    return tuple(map(list, zip(*lines)))
+
+
 def main():
     print("Hello World!")
+    # l1, l2 = read_input("sample-part1.txt")
+    l1, l2 = read_input("input.txt")
+    result = sum(map(lambda x: abs(x[0] - x[1]), list(zip(sorted(l1), sorted(l2)))))
+    print(result)
 
 
 if __name__ == "__main__":
