8f3d965b7ff5fd4e92baa1832fd9e1c0b5e70533
olivier <olibitter@gmail.com>
2024-12-02 06:24:23 +0100

    day-02: Solve part 1

---
+++ day-02/part1.py
@@ -0,0 +1,36 @@
+from itertools import pairwise
+
+
+def read_input(filename: str) -> list[list[int]]:
+    with open(filename) as file:
+        lines = [list(map(int, line.split())) for line in file]
+    return lines
+
+
+def diffs(xs: list[int]):
+    return [x1 - x2 for x1, x2 in pairwise(xs)]
+
+
+def is_inc(diffs: list[int]) -> bool:
+    return all(map(lambda x: x > 0, diffs))
+
+
+def is_dec(diffs: list[int]) -> bool:
+    return all(map(lambda x: x < 0, diffs))
+
+
+def is_safe(diffs: list[int]) -> bool:
+    return (is_inc(diffs) or is_dec(diffs)) and all(
+        map(lambda d: abs(d) in range(1, 4), diffs)
+    )
+
+
+def main():
+    # lines = read_input("sample-part1.txt")
+    lines = read_input("input.txt")
+    result = sum(map(is_safe, map(diffs, lines)))
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
