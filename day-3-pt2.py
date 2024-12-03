59f47e7730734a320c658af0fbc10f9adfdad645
zenoli <zenoli@users.noreply.github.com>
2024-12-03 12:27:59 +0100

    Create day-3-pt2.py

---
+++ day-3-pt2.py
@@ -0,0 +1,38 @@
+import re
+from ast import literal_eval
+
+
+def read_input(filename: str) -> str:
+    with open(filename) as file:
+        lines = [line for line in file]
+    return "".join(lines)
+
+
+def solve(line):
+    matches = re.findall(r"(?<=mul)\(\d+,\d+\)|don't\(\)|do\(\)", line)
+    print(matches)
+
+    result = 0
+
+    state = True
+
+    for match in matches:
+        if match == "don't()":
+            state = False
+        elif match == "do()":
+            state = True
+        elif state:
+            x, y = literal_eval(match)
+            result += x * y
+    return result
+
+
+def main():
+    # lines = read_input("sample-part1.txt")
+    lines = read_input("input.txt")
+    result = sum(map(solve, lines))
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
