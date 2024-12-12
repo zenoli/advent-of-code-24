42a121be3ea9cf75c1067103bb150473060c5d91
olivier <olibitter@gmail.com>
2024-12-13 00:18:23 +0100

    day-12: Part 2 (wip)

--- day-12/part2.py
+++ day-12/part2.py
@@ -116,13 +116,62 @@ def main():
                 corners[x + 1][y] = True
                 corners[x + 1][y + 1] = True
 
+    def get(pos: Position) -> str:
+        if not in_bounds(pos):
+            return "#"
+        else:
+            return grid[pos[0]][pos[1]]
+
+    def is_visited(pos: Position) -> int:
+        return visited[pos[0]][pos[1]]
+
+    def visit(pos):
+        visited[pos[0]][pos[1]] = True
+
+    def get_neighbors(pos: Position) -> Iterable[Position]:
+        return (add(pos, dir) for dir in DIRECTIONS)
+
+    def in_bounds(pos: Position) -> bool:
+        return 0 <= pos[0] < x_size and 0 <= pos[1] < y_size
+
+    def count_corners(pos: Position) -> int:
+        x = 2 * pos[0] + 1
+        y = 2 * pos[1] + 1
+
+        top_left = corners[x][y]
+        top_right = corners[x][y + 1]
+        bot_left = corners[x + 1][y]
+        bot_right = corners[x + 1][y + 1]
+        return sum([top_left, top_right, bot_left, bot_right])
+
+    def price(start: Position) -> int:
+        next_positions = {start}
+
+        area = 0
+        corners = 0
+
+        while len(next_positions) > 0:
+            pos = next_positions.pop()
+            symbol = get(pos)
+            if not is_visited(pos):
+                visit(pos)
+                area += 1
+                corners += count_corners(pos)
+                for neighbor in get_neighbors(pos):
+                    if get(neighbor) != symbol:
+                        continue
+                    else:
+                        if not is_visited(neighbor):
+                            next_positions.add(neighbor)
+        return area * corners
+
     def debug_corners():
         for line in corners:
             print("".join(["o" if c else "." for c in line]))
 
-    grid = read_input("sample.txt")
-    double_grid = get_double_grid(grid)
+    grid = read_input("sample5.txt")
     # grid = read_input("input.txt")
+    double_grid = get_double_grid(grid)
 
     x_size = len(grid)
     y_size = len(grid[0])
@@ -131,6 +180,7 @@ def main():
     y_size_double = len(double_grid[0])
 
     corners = [[False] * y_size_double for _ in range(x_size_double)]
+    visited = [[False] * y_size for _ in range(x_size)]
 
     debug(grid)
     print("")
@@ -140,6 +190,13 @@ def main():
 
     print("")
     debug_corners()
+    result = sum(
+        price(pos)
+        for pos in product(range(x_size), range(y_size))
+        if not is_visited(pos)
+    )
+
+    print(result)
 
 
 if __name__ == "__main__":
