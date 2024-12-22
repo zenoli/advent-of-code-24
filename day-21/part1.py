a542ac8e4a8c12ec1dc98971962476ef3c038b16
olivier <olibitter@gmail.com>
2024-12-22 10:18:08 +0100

    day-21: solve part 1

--- day-21/part1.py
+++ day-21/part1.py
@@ -1,5 +1,10 @@
+from functools import cache
+from collections import defaultdict
 from heapq import heappop, heappush
-from itertools import pairwise
+from itertools import pairwise, product
+
+type Graph = dict[str, list[tuple[str, str]]]
+type PredecessorMap = Graph
 
 
 def read_input(filename: str) -> list[str]:
@@ -29,14 +34,15 @@ numpad_graph = {
 keypad_graph = {
     "A": [("^", "<"), (">", "v")],
     "^": [("A", ">"), ("v", "v")],
-    ">": [("v", ">")],
+    "<": [("v", ">")],
     "v": [("<", "<"), ("^", "^"), (">", ">")],
+    ">": [("A", "^"), ("v", "<")],
 }
 
 
-def get_btn_presses(start: str, end: str, graph: dict[str, list[tuple[str, str]]]):
-    infinity = 1000
-    pred = {}
+def get_all_shortest_paths(start: str, end: str, graph: Graph) -> list[str]:
+    infinity = 1_000_000
+    pred: dict[str, list[tuple[str, str]]] = defaultdict(list)
     shortest_paths = {b: 0 if b == start else infinity for b in graph.keys()}
 
     visited = {start}
@@ -52,32 +58,65 @@ def get_btn_presses(start: str, end: str, graph: dict[str, list[tuple[str, str]]
             alt = shortest_paths[button] + 1
             if alt < shortest_paths[neighbor]:
                 shortest_paths[neighbor] = alt
-                pred[neighbor] = (button, dir)
+                pred[neighbor].append((button, dir))
                 heappush(queue, (alt, neighbor))
+            elif alt == shortest_paths[neighbor]:
+                pred[neighbor].append((button, dir))
 
-    next = end
-    btn_presses = ""
-    while next != start:
-        next, d = pred[next]
-        btn_presses = d + btn_presses
-    return btn_presses
+    def construct_shortest_paths(node: str, pred: PredecessorMap) -> list[str]:
+        if node == start:
+            return [""]
+
+        paths = []
+        for p, dir in pred[node]:
+            tmp = construct_shortest_paths(p, pred)
+            paths.extend(path + dir for path in tmp)
+
+        return paths
+
+    return [p + "A" for p in construct_shortest_paths(end, pred)]
 
 
-def get_numpad_sequence(code: str):
-    return (
-        "A".join(
-            get_btn_presses(start, end, numpad_graph)
-            for start, end in pairwise("A" + code)
-        )
-        + "A"
-    )
+def combine(xss: list[list[str]]) -> list[str]:
+    if len(xss) == 1:
+        return xss[0]
+
+    xs, *xss = xss
+    ys = combine(xss)
+    return [x + y for x, y in product(xs, ys)]
+
+
+def get_shortest_seq(seqs: list[str]):
+    return min(zip(map(len, seqs), seqs))[1]
+
+
+def get_sequence(seq: str, graph: Graph, level: int):
+    shortest_paths_list = [
+        get_all_shortest_paths(start, end, graph) for start, end in pairwise("A" + seq)
+    ]
+
+    combined_shortest_paths = combine(shortest_paths_list)
+    if level == 0:
+        return combined_shortest_paths[0]
+    seqs = []
+    for combined_shortest_path in combined_shortest_paths:
+        seqs.append(get_sequence(combined_shortest_path, keypad_graph, level - 1))
+
+    shortest_seq = get_shortest_seq(seqs)
+    return shortest_seq
 
 
 def main():
-    codes = read_input("sample.txt")
-    sequence = get_numpad_sequence("029A")
-    assert sequence == "<A^A>^^AvvvA"
-    print(sequence)
+    # codes = read_input("sample.txt")
+    codes = read_input("input.txt")
+    result = 0
+    for code in codes:
+        seq = get_sequence(code, numpad_graph, level=2)
+        num = int(code[:-1])
+        print(f"{len(seq)} * {num}")
+        result += num * len(seq)
+
+    print(result)
 
 
 if __name__ == "__main__":
